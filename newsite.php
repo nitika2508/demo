ec49537f4158932f4750d6e40939bf12d6820b32
nitika2508 <nitika.tiwari@newtechfusion.com>
2018-08-02 15:13:29 +0530

    added a new footer [issue 53]

---
+++ newsite.php
@@ -0,0 +1,64 @@
+http://new.trainingtweaks.nl/wp-content/uploads/2017/09/IMG_0661-e1496477363975-1024x448.jpg
+
+
+<ul><li class="address"><span class="icon"><i class="icon-location"></i></span><span class="address_wrapper">Training Tweaks<br>
+Groot-Essenburg 56<br>1081 HH Amsterdam<br>Nederland</span></li>
+<li class="phone phone-1"><span class="icon"><i class="icon-phone"></i></span><span>+31 (0) 629210742</span></li>
+<li class="mail"><span class="icon"><i class="icon-mail"></i></span><span>info@trainingtweaks.nl</span></li>
+</ul>
+
+
+<div class="contact-block-footer-section"><p><span class="icon"><i class="icon-location"></i></span></p>
+<span>Training Tweaks
+Groot-Essenburg 56
+1081 HH Amsterdam
+Nederland</span>
+<p><span class="icon"><i class="icon-phone"></i></span></p><span>+31 (0) 629210742</span>
+<p><span class="icon"><i class="icon-mail"></i></span></p><span>info@trainingtweaks.nl</span>
+</div>
+
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d2438.179710691543!2d4.866944102479818!3d52.330885827354344!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sTraining+Tweaks+Groot-Essenburg+56+1081+HH+Amsterdam+Nederland!5e0!3m2!1sen!2sin!4v1524666009586" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+
+
+<div class="contact-block-footer-section"><i class="icon-location"></i>
+Training Tweaks
+Groot-Essenburg 56
+1081 HH Amsterdam
+Nederland
+<i class="icon-phone"></i>+31 (0) 629210742
+<i class="icon-mail"></i>info@trainingtweaks.nl</div>
+
+
+trainjq165 04o3p2e4
+
+[gallery ids="639,120,123,432,403,404"]
+
+#Side_slide .extras {
+    display: none;
+} 
+
+
+<p>Ben jij op zoek naar een individuele hardlooptraining? Met mijn kennis en ervaring vanuit de sportpraktijk, de bewegingswetenschappen, de fysiotherapie, het onderwijs én een opleiding tot hardlooptrainer bied ik jou drie verschillende varianten om te werken aan techniek en/of kracht- &amp; conditie.</p>
+
+
+
+[su_slider source="media: 639,120,123,432,403,404" width="600" height="200" title="no" pages="no" autoplay="2000" speed="500"]
+
+[su_carousel source="media: 639,120,123,432,403,404" limit="20" link="none" target="self" width="800" height="100" responsive="yes" items="4" scroll="1" title="no" centered="yes" arrows="yes" pages="no" mousewheel="yes" autoplay="5000" speed="600" class=""]
+
+
+
+{$billingName}<br />
+
+{$billingName}
+
+fade-effect-contact bg-contain
+
+CIVICRM DATABASE:-
+database server:10.128.0.200:14682
+USER: pantheon
+PASS: ea6ad9a7df32481e93dda9a7ca1f2e3e
+
+DB name:-pantheon
